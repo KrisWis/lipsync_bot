473092caa7fd231a8c66a1b59c9199b9cf3cb10f
KrisWis <94256853+KrisWis@users.noreply.github.com>
2024-01-06 11:55:01 +0300

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,15 +1,15 @@
-TOKEN = '6104540822:AAEz3XdcLSUxb6M_5eS7lR9pVG0YfqgVnhw'
-CRYPTO_BOT_TOKEN = '140175:AAIZeAXGQ57mjhdUJDD3dww9fW4vOLxuDnJ'
+TOKEN = ''
+CRYPTO_BOT_TOKEN = ''
 
 pg = {
     'host': '127.0.0.1',
     'user': 'postgres',
-    'password': '1234',
+    'password': '',
     'database': 'lipsync_test'
 }
 
-convertio = '9a524a88300faf212fa78cd61befc087'
-elevenlabs = "42701a832c04fc9b63a7c2c751f47945"
-sync_lab = "ceb19a50-98cb-4594-b92f-bfde8eaaeb86"
+convertio = ''
+elevenlabs = ""
+sync_lab = ""
 
 admins = [1979922062, 780270834, 6455916237]
